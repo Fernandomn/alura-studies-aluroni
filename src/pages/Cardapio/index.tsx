cdd3ccea0960e7276a401916f1fb385eb66e6ac8
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-11-26 15:40:52 -0300

    setting up header

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,16 @@
+import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
+
+export default function Cardapio() {
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+
+      <header className={styles.header}>
+        <div className={styles.header__text}>A Casa do Código e da Massa</div>
+      </header>
+    </main>
+  );
+}
