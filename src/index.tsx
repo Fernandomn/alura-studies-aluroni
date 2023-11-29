fdf262d86447a6d563c55bc601ed6126a293b493
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-11-28 21:20:01 -0300

    implementing react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
+import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'normalize.css';
+import AppRouter from 'routes/routes';
 import './index.css';
-import Cardapio from './pages/Cardapio';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Cardapio />
+    <AppRouter />
   </React.StrictMode>
 );
