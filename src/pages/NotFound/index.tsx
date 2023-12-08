d3c7ff428c0607488184711973256f8f87d9fc10
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-12-08 18:07:00 -0300

    creating "Voltar" (go back)  button

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,9 +1,12 @@
 import { ReactComponent as NotFoundImage } from 'assets/not_found.svg';
 import classNames from 'classnames';
+import { useNavigate } from 'react-router-dom';
 import themeStyles from 'styles/Theme.module.scss';
 import styles from './NotFound.module.scss';
 
 export default function NotFound() {
+  const navigate = useNavigate();
+
   return (
     <div
       className={classNames({
@@ -11,6 +14,9 @@ export default function NotFound() {
         [themeStyles.container]: true,
       })}
     >
+      <div className={styles.voltar}>
+        <button onClick={() => navigate(-1)}>{'< Voltar'}</button>
+      </div>
       <NotFoundImage />
     </div>
   );
